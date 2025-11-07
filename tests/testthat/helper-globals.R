a453beb5155b3a75d19d01740787439595e5eb46
andrei-stoica26 <andreistoica@foxmail.com>
2025-11-07 12:47:39 +0800

    Started adding tests

---
+++ tests/testthat/helper-globals.R
@@ -0,0 +1,4 @@
+scePath <- system.file('extdata', 'sceObj.qs', package='scLang')
+sceObj <- qs::qread(scePath)
+seuratPath <- system.file('extdata', 'seuratObj.qs', package='scLang')
+seuratObj <- qs::qread(seuratPath)
---
+++ tests/testthat/test-scLang.R
@@ -0,0 +1,29 @@
+test_that("compatibility functions work", {
+    expect_equal(metadataNames(sceObj), c('Mutation_Status',
+                                          'Cell_Cycle',
+                                          'Treatment',
+                                          'sizeFactor',
+                                          'Cluster',
+                                          'Donor'))
+
+    expect_error(metadataNames(c(1, 2, 3)))
+    expect_equal(length(scCol(sceObj, 'Cell_Cycle')), 200)
+
+    expect_equal(scColCounts(sceObj, 'Cell_Cycle')['G2M'],
+                 setNames(56, 'G2M'))
+    expect_equal(scColPairCounts(sceObj, 'Mutation_Status', 'Cell_Cycle')[1, 3],
+                 24)
+
+    expect_equal(length(scGeneExp(sceObj, 'Gene_0480')), 200)
+    expect_equal(dim(scExpMat(sceObj)), c(500, 200))
+
+    v <- scPCAMat(sceObj)
+    w <- scPCAMat(seuratObj)
+    colnames(v) <- paste0('PC_', seq(50))
+    expect_equal(v, w)
+
+    v <- scUMAPMat(sceObj)
+    w <- scUMAPMat(seuratObj)
+    colnames(v) <- paste0('UMAP_', seq(2))
+    expect_equal(v, w)
+})
