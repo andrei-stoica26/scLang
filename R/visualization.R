88bccfcfaf5986b092f88f306440871295ca702f
andrei-stoica26 <andreistoica@foxmail.com>
2025-12-16 16:49:28 +0800

    Added featurePlot

--- R/visualization.R
+++ R/visualization.R
@@ -1,6 +1,8 @@
-#' @importFrom ggplot2 aes element_text ggplot geom_point labs scale_color_manual theme theme_classic
+#' @importFrom ggplot2 aes element_text ggplot geom_point labs scale_color_gradientn scale_color_manual theme theme_classic
 #' @importFrom grDevices rainbow
 #' @importFrom henna centerTitle
+#' @importFrom methods is
+#' @importFrom wesanderson wes_palette
 #'
 NULL
 
@@ -9,7 +11,6 @@ NULL
 #' This function creates a dimensionality reduction plot
 #'
 #' @inheritParams documentFun
-#' @param title Plot title.
 #' @param paletteFun Palette function. Must accept the number of colors as the
 #' first argument and require no other arguments.
 #' @param noGroupsLegendLab Legend label to be used when no grouping is
@@ -32,7 +33,7 @@ dimPlot <- function(scObj,
                     legendTitle = 'Group',
                     noGroupsLegendLab = 'Object',
                     paletteFun = rainbow,
-                    alpha=0.7,
+                    alpha=0.6,
                     legendPos = c('right', 'top', 'left', 'bottom'),
                     legendTextSize = 10,
                     legendTitleSize = 10,
@@ -68,3 +69,74 @@ dimPlot <- function(scObj,
     p <- centerTitle(p, title, ...)
     return(p)
 }
+
+#' Creates a dimensionality reduction plot
+#'
+#' This function creates a dimensionality reduction plot
+#'
+#' @inheritParams dimPlot
+#' @param feature A gene name or metadata column name.
+#' @param palette Color palette.
+#'
+#' @return A feature plot.
+#'
+#' @examples
+#' scePath <- system.file('extdata', 'sceObj.qs2', package='scLang')
+#' sceObj <- qs2::qs_read(scePath)
+#' featurePlot(sceObj, 'Gene_0289')
+#'
+#' @export
+#'
+featurePlot <- function(scObj,
+                        feature = rownames(scObj)[1],
+                        title = feature,
+                        dimred = 'umap',
+                        dims = c(1, 2),
+                        legendTitle = feature,
+                        palette = wes_palette('Royal1')[c(3, 2)],
+                        alpha = 0.6,
+                        legendPos = c('right', 'top', 'left', 'bottom'),
+                        legendTextSize = 10,
+                        legendTitleSize = 10,
+                        axisTextSize = 12,
+                        axisTitleSize = 12,
+                        ...){
+
+    legendPos <- match.arg(legendPos, c('right', 'top', 'left', 'bottom'))
+    dimred <- dimredName(scObj, dimred)
+    df <- as.data.frame(scDimredMat(scObj, dimred)[, dims])
+
+    if (feature %in% rownames(scObj))
+        df[, 3] <- scExpMat(scObj, genes=feature)[, 1]
+
+    if (feature %in% metadataNames(scObj)){
+        col <- scCol(scObj, feature)
+        if (is(col)[1] != 'numeric')
+            stop(feature, ' is not a numeric column.')
+        df[, 3] <- col
+    }
+
+    if(ncol(df) == 2)
+        stop('Feature not found: ', feature, '.')
+
+    colnames(df)[3] <- feature
+
+    p <- ggplot(df) + geom_point(aes(x=df[, 1],
+                                     y=df[, 2],
+                                     color=df[, 3]),
+                                 alpha=alpha) +
+        labs(x=paste0(dimred, '_', dims[1]),
+             y=paste0(dimred, '_', dims[2]),
+             color=legendTitle) +
+        scale_color_gradientn(colors=palette) +
+        theme_classic() +
+        theme(legend.position=legendPos,
+              legend.text=element_text(size=legendTextSize),
+              legend.title=element_text(size=legendTitleSize),
+              axis.text=element_text(size=axisTextSize),
+              axis.title=element_text(size=axisTitleSize))
+
+    p <- centerTitle(p, title, ...)
+    return(p)
+}
+
